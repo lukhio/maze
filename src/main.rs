b34484241a1466c6ade8a912cdc78c506d39329e
Julien Gamba <julien@jgamba.eu>
2023-05-19 15:54:54 +0200

    Comments

--- src/main.rs
+++ src/main.rs
@@ -22,22 +22,29 @@ where T: sdl2::render::RenderTarget {
 }
 
 pub fn main() {
+    // Init
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("rust-sdl2 demo", SCREEN_WIDTH, SCREEN_HEIGHT)
+    let window = video_subsystem.window("maze", SCREEN_WIDTH, SCREEN_HEIGHT)
         .position_centered()
         .build()
         .unwrap();
 
     let mut canvas = window.into_canvas().build().unwrap();
 
+    // Draw background in black
     canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.clear();
     canvas.present();
+
+    // Draw rectangles in white
     canvas.set_draw_color(Color::RGB(255, 255, 255));
+
+    // Main loop
     let mut event_pump = sdl_context.event_pump().unwrap();
     'running: loop {
+        // Draw white rectangles
         for i in (0..SCREEN_HEIGHT).step_by(SQUARE_SIZE as usize) {
             for j in (0..SCREEN_WIDTH).step_by(SQUARE_SIZE as usize) {
                 if i == j {
@@ -46,6 +53,7 @@ pub fn main() {
             }
         }
 
+        // Wait for events
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
@@ -55,7 +63,6 @@ pub fn main() {
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
 
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
